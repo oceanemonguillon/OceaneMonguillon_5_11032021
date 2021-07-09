5c3b2be449318d1e38af810beb5eca613fb2d341
oceane monguillon <oceane.monguillon@gmail.com>
2021-07-09 12:59:15 +0200

    message d'erreur chargement bdd

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 //Recherche de la balise <ul> dans le HTML
 const ul = document.getElementById('bears');
-//Fonction pour créer un nouvel élément de la liste
-function nouvelElement(element) {
-    return document.createElement(element);
-}
+
 //Fonction pour ajouter un élément enfant à l'élément parent
 function append(parent, child) {
     return parent.appendChild(child);
@@ -13,19 +10,21 @@ function append(parent, child) {
 fetch('http://localhost:3000/api/teddies/') 
     //retourne une promesse et transforme les données en JSON si elles sont valides
     .then(function(res) { 
-      if (res.ok) {
-        return res.json();
-      }
+        if (res.ok) {
+            return res.json();
+        }else{
+            alert("Une erreur est survenue, réessayez plus tard!");
+        }
     })
     //Traitement des données JSON
     .then(function(data){
-        //création des elements de la liste grace à la fonction nouvelElement
+        //création de chacun des elements de la liste
         return data.map(function(bear){
-            let li = nouvelElement('li');
-            let img = nouvelElement('img');
-            let spanName = nouvelElement('span');
-            let spanPrice = nouvelElement('span');
-            let spanDescription= nouvelElement('span');
+            let li = document.createElement('li');
+            let img = document.createElement('img');
+            let spanName = document.createElement('span');
+            let spanPrice = document.createElement('span');
+            let spanDescription= document.createElement('span');
 
             //Ajoute des classes à l'élément parent <li>
             li.classList.add("list-group-item", "flex-column", "align-item-start");  
